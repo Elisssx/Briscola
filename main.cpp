011f8ef344d06fbd53d80c978a2fe485f185650b
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-04-15 14:08:24 +0200

    Update main.cpp
    Riorganizzate le struct

--- main.cpp
+++ main.cpp
@@ -3,36 +3,24 @@
 #include <time.h>
 using namespace std;
  
-struct Carta 
-{
+struct Carta {
     string carta; // es. asso…valori numerici per capire quale carta è piu alta?
-    int valore;
     string seme;
+    int valore;
     bool briscola;
     bool uscita; // La carta è già uscita?
     bool distribuita; // La carta è già stata distribuita?
 };
-struct Mano
-{
-    string semeIniziale; // Il seme che è stato giocato per prima nella mano
+struct Partita {
+    string briscola; // Seme di briscola della partita corrente
+    int giocatore; // punteggio Giocatore
+    int computer; // punteggio Computer
     Carta carteGiocatore[20]; // Carte utente
     Carta carteComputer[20]; // Carte computer
-    Carta cartaATerra; // Carta che è appena stata gettata dal computer
-    bool giocatore; // Quale giocatore ha giocato per primo in questa mano? True se è il computer, False se è l'umano.
-};
-struct Partita
-{
-    string briscola; // Seme di briscola della partita corrente
-    int giocatore; // punteggio Giocatore che ha giocato per primo
-    int computer; // punteggio Computer
     Carta mazzo[40]; // 40 carte totali
-    Mano manoCorrente; // Mano corrente
+    Carta cartaATerra; // Carta che è appena stata gettata dal computer
+    int _contatoreComputer; // Contatore carte computer
 };
- 
-void spiegazionebriscola()
-{
-    cout << "Cicucia";
-}
 
 
 void inizializzaMazzo(Partita &m)
@@ -90,9 +78,8 @@ void inizializzaMazzo(Partita &m)
             m.mazzo[i].carta = "Re";
             m.mazzo[i].valore=4;
         }
-        // cosi per tutte le carte
         if (i%4 == 0)
-            m.mazzo[i].seme = "Spada";
+            m.mazzo[i].seme = "Spade";
         if (i%4 == 1)
             m.mazzo[i].seme = "Bastoni";
         if (i%4 == 2)
@@ -101,12 +88,17 @@ void inizializzaMazzo(Partita &m)
             m.mazzo[i].seme = "Coppe";
     }
     // Dice se è una briscola
-    for (int i=0; i<40; i++) 
-    {
-        m.mazzo[i].briscola = false;
-        if (m.mazzo[i].seme==m.briscola)
-            m.mazzo[i].briscola=true;
-    }
+    for (int i=0; i<40; i++)
+        m.mazzo[i].briscola = (m.mazzo[i].seme == m.briscola);
+}
+
+
+void _debugMazzo(Partita &m)
+{
+    string s = "          ";
+    for (int i=0; i<40; i++)
+        cout << m.mazzo[i].carta << s << m.mazzo[i].seme << s << m.mazzo[i].valore << s << m.mazzo[i].briscola << s << m.mazzo[i].uscita << s << m.mazzo[i].distribuita << endl;
+    system("pause");
 }
 
 
@@ -114,19 +106,23 @@ void inizializzaMazzo(Partita &m)
 void dividiMazzo(Partita &p)
 {
     // A questo punto il mazzo è tutto dentro p.mazzo, lungo 40 e ordinato
+    srand(time(0)); // Inizializza il generatore di numeri casuali
+    int x; // Variabile per il ciclo for
     for (int i=0; i<20; i++) {
-        int x = rand()%40; //Numero da 0 a 39
-        while (p.mazzo[x].distribuita) { // Finché è già stata distribuita
+        x = rand()%40; // Numero da 0 a 39
+        while (p.mazzo[x].distribuita) // Finché è già stata distribuita
             x = rand()%40;
-        }
-        p.manoCorrente.carteComputer[i] = p.mazzo[x];
+        p.carteComputer[i] = p.mazzo[x];
         p.mazzo[x].distribuita = true;
-        while (p.mazzo[x].distribuita) { // Finché è già stata distribuita
+
+        x = rand()%40;
+        while (p.mazzo[x].distribuita) // Finché è già stata distribuita
             x = rand()%40;
-        }
-        p.manoCorrente.carteGiocatore[i] = p.mazzo[x];
+        p.carteGiocatore[i] = p.mazzo[x];
         p.mazzo[x].distribuita = true;
     }
+
+    _debugMazzo(p);
 }
 
 
@@ -134,9 +130,9 @@ void dividiMazzo(Partita &p)
 bool controllo(Partita &p) {
     bool esaurito = true;
     for (int i = 0; i < 20; i++) {
-        if (!p.manoCorrente.carteComputer[i].uscita) // ! significa NOT
+        if (!p.carteComputer[i].uscita) // ! significa NOT
             esaurito = false;
-        if (!p.manoCorrente.carteGiocatore[i].uscita)
+        if (!p.carteGiocatore[i].uscita)
             esaurito = false;
     }
     return esaurito;
@@ -145,11 +141,9 @@ bool controllo(Partita &p) {
 
 // Funzione che fa gettare una carta al computer e che guarda se non è gia uscita
 void computer(Partita &p) {
-    int i = 0;
-    while (p.manoCorrente.carteComputer[i].uscita)
-        i++;
-    p.manoCorrente.carteComputer[i].uscita = true;
-    p.manoCorrente.cartaATerra = p.manoCorrente.carteComputer[i];
+    p.carteComputer[p._contatoreComputer].uscita = true;
+    p.cartaATerra = p.carteComputer[p._contatoreComputer];
+    p._contatoreComputer++;
 }
 
 
@@ -157,29 +151,29 @@ void computer(Partita &p) {
 int stampaCarteGiocatore(Partita &p) {
     system("cls"); // Cancella lo schermo
     cout << p.computer << " - " << p.giocatore << "   (Briscola: " << p.briscola << ")" << endl << endl;
-    cout << "COMPUTER: " << p.manoCorrente.cartaATerra.carta << " di " << p.manoCorrente.cartaATerra.seme << " (valore: " << p.manoCorrente.cartaATerra.valore << ')' << endl << endl;//mette la carta buttata dal giocatore precendente(computre)
+    cout << "COMPUTER: " << p.cartaATerra.carta << " di " << p.cartaATerra.seme << " (valore: " << p.cartaATerra.valore << ')' << endl << endl;//mette la carta buttata dal giocatore precendente(computre)
 	for (int i=0;i<3;i++) {  
-		if (!p.manoCorrente.carteGiocatore[i].uscita)//se non è uscita fa stampare
-            cout << i+1 << ")" << p.manoCorrente.carteGiocatore[i].carta << " di " << p.manoCorrente.carteGiocatore[i].seme << " (valore: " << p.manoCorrente.carteGiocatore[i].valore << ')' << endl;
+		if (!p.carteGiocatore[i].uscita)//se non è uscita fa stampare
+            cout << i+1 << ") " << p.carteGiocatore[i].carta << " di " << p.carteGiocatore[i].seme << " (valore: " << p.carteGiocatore[i].valore << ')' << endl;
         else
-            cout << i+1 << ")" << "<Carta uscita>" << endl;
+            cout << i+1 << ") " << "<Carta uscita>" << endl;
     }
     cout<<"Carta: ";
     int scelta; // Carta scelta dal giocatore
     cin >> scelta;
-    while (scelta<1 || scelta>3 || p.manoCorrente.carteGiocatore[scelta-1].uscita) {//finchè  non è gia uscita continua a richiederla oppure hai scelto un numero diverso dal 1 2 3 
+    while (scelta<1 || scelta>3 || p.carteGiocatore[scelta-1].uscita) {//finchè  non è gia uscita continua a richiederla oppure hai scelto un numero diverso dal 1 2 3 
         cout<<"Carta: ";
         cin >> scelta;
     }
-    p.manoCorrente.carteGiocatore[scelta-1].uscita = true;
+    p.carteGiocatore[scelta-1].uscita = true;
     return scelta-1; // Carta corretta da mettere nell'array   
 }
 
 
 // Funzione che scala tutte le carte al di sotto di sceltax in alto di una posizione
-int scaloCarte(Partita &p, int lunghezza,int sceltax) {
+void scaloCarte(Partita &p, int lunghezza, int sceltax) {
     for(int i=sceltax; i<lunghezza-1; i++)// Tira tutte le carte indietro di uno 
-        p.manoCorrente.carteGiocatore[i] = p.manoCorrente.carteGiocatore[i+1];
+        p.carteGiocatore[i] = p.carteGiocatore[i+1];
 }
 
 
@@ -206,37 +200,32 @@ bool _vinceS(Carta terra, Carta sfidante) {
 // Funzione che assegna i punti
 void assegnaPunti(Partita &p, int s) {
     // Se la carta sfidante vince la carta a terra, allora il giocatore guadagna un punto
-    if (_vinceS(p.manoCorrente.cartaATerra, p.manoCorrente.carteGiocatore[s]))// Se la carta del giocatore batte quella del computer
-        p.giocatore += p.manoCorrente.cartaATerra.valore + p.manoCorrente.carteGiocatore[s].valore;//se _vinceS ritorna true i punti vanno al giocatore
+    if (_vinceS(p.cartaATerra, p.carteGiocatore[s]))// Se la carta del giocatore batte quella del computer
+        p.giocatore += p.cartaATerra.valore + p.carteGiocatore[s].valore;//se _vinceS ritorna true i punti vanno al giocatore
     else
-        p.computer += p.manoCorrente.cartaATerra.valore + p.manoCorrente.carteGiocatore[s].valore;//se _vinceS ritorna false i punti vanno al PC
+        p.computer += p.cartaATerra.valore + p.carteGiocatore[s].valore;//se _vinceS ritorna false i punti vanno al PC
 }
 
 
 int main() 
 {
 	int istruzioni;
-    cout<<"Benvenuto su briscola"<<endl;
-    cout<<"inserisci nome utente"<<endl;
+    cout << "Benvenuto su Briscola"<<endl;
+    cout << "Username: ";
     string utente;
-    cin>>utente;
-	cout<<"Puoi scoprire le regole del gioco digitando '1'.  In alternativa puoi iniziare una nuova partita, digitando '2'"<<endl;
-    cin>>istruzioni;
-    if(istruzioni==1)
-        spiegazionebriscola();
-    else if(istruzioni!=2)
-    	return 0;
+    cin >> utente;
 
     // mazzo semi valori
     Partita m;
-    m.giocatore = 0;//azzera punteggi
+    m.giocatore = 0; // Azzera i punteggi
     m.computer = 0;
+    m._contatoreComputer = 0;
 
   	//random seme primario
     srand(time(0)); // Rende casuale la generazione di numeri casuali con rand() sulla base dell'orario corrente
     int s = rand()%4; // Genera un numero casuale tra 0 e 3
     if (s == 0)
-        m.briscola = "Spada";
+        m.briscola = "Spade";
     else if (s == 1)
         m.briscola = "Bastoni";
     else if (s == 2)
